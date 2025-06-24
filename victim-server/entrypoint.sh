32679be787534dbf1fdd1d8d307e35975899594f
Catherine Chen <catherine.chen.canadait@gmail.com>
2025-06-23 22:35:59 -0600

    Update: fix Wazuh to show on the Kibana interface

--- victim-server/entrypoint.sh
+++ victim-server/entrypoint.sh
@@ -5,13 +5,21 @@ WAZUH_MANAGER_IP="172.21.0.4"
 echo "WAZUH_MANAGER_IP is: $WAZUH_MANAGER_IP"
 
 echo "Replace ossec.conf's MANAGER_IP with ${WAZUH_MANAGER_IP}..."
+cp /config/wazuh_manager.conf /var/ossec/etc/ossec.conf
 sed -i "s/MANAGER_IP/${WAZUH_MANAGER_IP}/" /var/ossec/etc/ossec.conf
 
 echo "Replaced ossec.conf："
 head -n 20 /var/ossec/etc/ossec.conf
 
+echo "Start rsyslog..."
+rsyslogd
+
+echo "Start SSH server..."
+service ssh start
+
 echo "Start agent-auth..."
 /var/ossec/bin/agent-auth -m ${WAZUH_MANAGER_IP} -p 1515
 
 echo "Start Wazuh Agent..."
-exec /var/ossec/bin/wazuh-agentd -f
+/var/ossec/bin/wazuh-control start
+tail -f /var/ossec/logs/ossec.log
